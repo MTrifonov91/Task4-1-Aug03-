12c6f2671963cfcc4cf1d1cd557952f6f40d97ce
Mihail Trifonov <trifonovmihail@mail.ru>
2018-08-05 22:56:18 +0300

    Added the Task4-1, task done

---
+++ Task 4-1/src/ShoppingCart/ShoppingCart.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ShoppingCart;
+
+/**
+ *
+ * @author User
+ */
+public class ShoppingCart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String A;
+        // Assign the message variable 
+        A = "This is a shoping cart";
+        // Print and run the codeString A;
+        System.out.println(A);
+        
+       
+    }
+    
+}
